39786b85874eef7b4118bd2301b79e3bab9ef527
Morgan Kpassi <morgan.kpassi@gmail.com>
2022-12-08 16:00:48 +0100

    Ajout des récupération des données

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const barCanvas = document.getElementById("barCanvas");
-const barChart = new Chart(barCanvas, {
-  type: "bar",
-  data: {
-    labels: ["Marseille", "Paris", "Lyon", "Reims"],
-    datasets: [
-      {
-        data: [],
-      },
-    ],
-  },
-});
+// const barCanvas = document.getElementById("barCanvas");
+// const barChart = new Chart(barCanvas, {
+//   type: "bar",
+//   data: {
+//     labels: ["Marseille", "Paris", "Lyon", "Reims"],
+//     datasets: [
+//       {
+//         data: [],
+//       },
+//     ],
+//   },
+// });
+
+fetch(
+  "https://data.culture.gouv.fr/api/v2/catalog/datasets/base-des-reliures-numerisees-de-la-bibliotheque-nationale-de-france"
+) //Récupere les données de l'API
+  .then((res) => res.json()) // Permet de lire le fichier.json
+  .then((data) => console.log(data));
