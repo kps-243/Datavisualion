c9d2f3ff0dbb2a1b9c132a253a1424f65288fbd2
Morgan Kpassi <morgan.kpassi@gmail.com>
2022-12-15 15:44:36 +0100

    Mise en com de la config et la prise des données

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-// const barCanvas = document.getElementById("barCanvas");
-// const barChart = new Chart(barCanvas, {
-//   type: "bar",
-//   data: {
-//     labels: ["Marseille", "Paris", "Lyon", "Reims"],
-//     datasets: [
-//       {
-//         data: [],
-//       },
-//     ],
-//   },
-// });
-
 fetch(
-  "https://data.culture.gouv.fr/api/v2/catalog/datasets/base-des-reliures-numerisees-de-la-bibliotheque-nationale-de-france"
+  "https://www.data.gouv.fr/fr/datasets/prix-des-carburants-en-france-flux-instantane/"
 ) //Récupere les données de l'API
   .then((res) => res.json()) // Permet de lire le fichier.json
   .then((data) => console.log(data));
+
+// //Pour la configuration
+// const config = {
+//   type: "line",
+//   data: data,
+//   options: {
+//     responsive: true,
+//     plugins: {
+//       title: {
+//         display: true,
+//         text: (ctx) => "Point Style: " + ctx.chart.data.datasets[0].pointStyle,
+//       },
+//     },
+//   },
+// };
+
+// //Pour prendre les données
+
+// const data = {
+//   labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6"],
+//   datasets: [
+//     {
+//       label: "Dataset",
+//       data: Utils.numbers({ count: 6, min: -100, max: 100 }),
+//       borderColor: Utils.CHART_COLORS.red,
+//       backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+//       pointStyle: "circle",
+//       pointRadius: 10,
+//       pointHoverRadius: 15,
+//     },
+//   ],
+// };
