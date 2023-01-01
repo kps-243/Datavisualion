9f3525b6344d586ef4ff063afba3369814941bd8
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-01-01 22:22:25 +0100

    Cherche à relier la library chartjs

--- app.js
+++ app.js
@@ -1,37 +1,14 @@
-fetch(
-  "https://www.data.gouv.fr/fr/datasets/prix-des-carburants-en-france-flux-instantane/"
-) //Récupere les données de l'API
-  .then((res) => res.json()) // Permet de lire le fichier.json
-  .then((data) => console.log(data));
+const barCanvas = document.getElementById("barCanvas").getContext("2d");
+const barCanvasData = {
+  type: "bar",
+  data: {
+    labels: ["Congo", "Nigeria", "Mali", "Maroc", "Tunisie"],
+  },
+  datasets: [
+    {
+      data: [200, 150, 240, 400, 120],
+    },
+  ],
+};
 
-// //Pour la configuration
-// const config = {
-//   type: "line",
-//   data: data,
-//   options: {
-//     responsive: true,
-//     plugins: {
-//       title: {
-//         display: true,
-//         text: (ctx) => "Point Style: " + ctx.chart.data.datasets[0].pointStyle,
-//       },
-//     },
-//   },
-// };
-
-// //Pour prendre les données
-
-// const data = {
-//   labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6"],
-//   datasets: [
-//     {
-//       label: "Dataset",
-//       data: Utils.numbers({ count: 6, min: -100, max: 100 }),
-//       borderColor: Utils.CHART_COLORS.red,
-//       backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-//       pointStyle: "circle",
-//       pointRadius: 10,
-//       pointHoverRadius: 15,
-//     },
-//   ],
-// };
+new Chart(barCanvas).Line(barCanvasData);
