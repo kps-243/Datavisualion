98cfd5828ef1e8b744634a564b23463f202e16be
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-01-04 20:26:40 +0100

    début du parcours du tableau pour accéder aux données

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-const ctx = document.getElementById("myChart");
+fetch(
+  "https://public.opendatasoft.com/api/records/1.0/search/?dataset=prix_des_carburants_j_7&q=&facet=cp&facet=pop&facet=city&facet=automate_24_24&facet=fuel&facet=shortage&facet=update&facet=services&facet=brand&refine.pop=R"
+) //Récupere les données de l'API
+  .then((res) => res.json()) // Permet de lire le fichier.json
+  .then((data) => console.log(data));
 
-new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  },
-});
+let prix = data.records;
+for (prix = 0; prix < prix.length; prix++) {
+  console.log(prix);
+}
