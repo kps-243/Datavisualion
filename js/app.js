921c41e9ab5407109260274bccde29d81d87b5c9
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-01-10 18:07:33 +0100

    ADD graphe moyenne prix

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+function average(arr) {
+  let sum = 0;
+
+  // Boucle à travers les éléments du tableau pour additionner les valeurs
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  // Retourne la somme divisée par le nombre d'éléments dans le tableau
+  return sum / arr.length;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(average(numbers));
+
 const apiUrl =
   "https://public.opendatasoft.com/api/records/1.0/search/?dataset=prix_des_carburants_j_7&q=&facet=cp&facet=pop&facet=city&facet=automate_24_24&facet=fuel&facet=shortage&facet=update&facet=services&facet=brand&refine.pop=R";
 
@@ -6,6 +21,7 @@ fetch(apiUrl) //Récupere les données de l'API
   .then((data) => {
     console.log(data);
     const ctx = document.getElementById("prixVille").getContext("2d");
+    const mid = document.getElementById("midPrice").getContext("2d");
     const myChart = new Chart(ctx, {
       type: "bar",
       data: {
@@ -43,6 +59,67 @@ fetch(apiUrl) //Récupere les données de l'API
               "rgba(75, 192, 192, 1)",
               "rgba(153, 102, 255, 1)",
               "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 2,
+          },
+        ],
+      },
+    });
+    const priceGazole = [
+      data.records[0].fields.price_gazole,
+      data.records[1].fields.price_gazole,
+      data.records[2].fields.price_gazole,
+      data.records[3].fields.price_gazole,
+      data.records[4].fields.price_gazole,
+      data.records[5].fields.price_gazole,
+    ];
+    const priceSp95 = [
+      data.records[1].fields.price_sp95,
+      data.records[2].fields.price_sp95,
+      data.records[3].fields.price_sp95,
+      data.records[4].fields.price_sp95,
+      data.records[5].fields.price_sp95,
+    ];
+    const priceSp98 = [
+      data.records[1].fields.price_sp98,
+      data.records[2].fields.price_sp98,
+      data.records[3].fields.price_sp98,
+      data.records[4].fields.price_sp98,
+      data.records[9].fields.price_sp98,
+    ];
+    const priceE10 = [
+      data.records[5].fields.price_e10,
+      data.records[9].fields.price_e10,
+    ];
+    const midPrice = new Chart(mid, {
+      type: "bar",
+      data: {
+        labels: [
+          data.facet_groups[5].facets[5].name,
+          data.facet_groups[5].facets[2].name,
+          data.facet_groups[5].facets[4].name,
+          data.facet_groups[5].facets[3].name,
+        ],
+        datasets: [
+          {
+            label: "Ventes",
+            data: [
+              average(priceGazole),
+              average(priceSp95),
+              average(priceSp98),
+              average(priceE10),
+            ], // Les données seront remplies lorsque la première requête à l'API sera effectuée
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
             ],
             borderWidth: 2,
           },
