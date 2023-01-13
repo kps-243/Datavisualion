0c0c07a071aa60b054640461fc1ad2451754b683
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-01-13 23:36:53 +0100

    UPD commentaire

--- js/app.js
+++ js/app.js
@@ -10,21 +10,26 @@ function average(arr) {
   return sum / arr.length;
 }
 
+// Il s'agit d'une fonction qui permet de calculer une moyenne
+// On l'utilise plus bas lorque du calcule des moyennes du pétrole
+
 const apiUrl =
   "https://public.opendatasoft.com/api/records/1.0/search/?dataset=prix_des_carburants_j_7&q=&facet=cp&facet=pop&facet=city&facet=automate_24_24&facet=fuel&facet=shortage&facet=update&facet=services&facet=brand&refine.pop=R";
 
-fetch(apiUrl) //Récupere les données de l'API
+fetch(apiUrl) //Récupere les données de l'API via la fonction fetch en ajax
   .then((res) => res.json()) // Permet de lire le fichier.json
   .then((data) => {
-    console.log(data);
     const ctx = document.getElementById("prixVille").getContext("2d");
     const mid = document.getElementById("midPrice").getContext("2d");
-    const nbs = document.getElementById("nbStation").getContext("2d");
+    const nbs = document.getElementById("nbStation").getContext("2d"); // permet de saisir les canvas afin de les modifier par chart.js
     const myChart = new Chart(ctx, {
-      type: "bar",
+      // création d'une chart dans le canva qui contient l'id prixVille contenu dans la constante ctx
+      type: "bar", // type barre
       data: {
+        // Permet d'afficher les données present sur le graphique
         labels: [
-          data.records[0].fields.city,
+          // Ce sont les donnéer que l'on va comparer
+          data.records[0].fields.city, // J'appelle directement le labels en allant le chercher directement dans la base de l'API
           data.records[1].fields.city,
           data.records[2].fields.city,
           data.records[3].fields.city,
@@ -32,10 +37,11 @@ fetch(apiUrl) //Récupere les données de l'API
           data.records[5].fields.city,
         ],
         datasets: [
+          // Cela va permettre d'afficher les valeurs pour chaque éléments
           {
             label: "Prix du gazole",
             data: [
-              data.records[0].fields.price_gazole,
+              data.records[0].fields.price_gazole, // Appelle direct des valeurs dans l'API
               data.records[1].fields.price_gazole,
               data.records[2].fields.price_gazole,
               data.records[3].fields.price_gazole,
@@ -43,6 +49,7 @@ fetch(apiUrl) //Récupere les données de l'API
               data.records[5].fields.price_gazole,
             ], // Les données seront remplies lorsque la première requête à l'API sera effectuée
             backgroundColor: [
+              // Ajout des couleures pour les bars
               "rgba(171, 58, 183, 0.6)",
               "rgba(127, 58, 183, 0.6)",
               "rgba(84, 58, 183, 0.6)",
@@ -52,6 +59,7 @@ fetch(apiUrl) //Récupere les données de l'API
               "rgba(58, 183, 183, 0.6)",
             ],
             borderColor: [
+              // Ajout couleures de bordure
               "rgba(171, 58, 183,1.0)",
               "rgba(127, 58, 183,1.0)",
               "rgba(84, 58, 183,1.0)",
@@ -66,10 +74,11 @@ fetch(apiUrl) //Récupere les données de l'API
       },
       options: {
         responsive: true,
-        maintainAspectRatio: false,
+        maintainAspectRatio: false, // Permet de rendre le canva responsive adaptable à tout les écrans
       },
     });
     const priceGazole = [
+      // Création d'une constante d'un tableau de valeurs
       data.records[0].fields.price_gazole,
       data.records[1].fields.price_gazole,
       data.records[2].fields.price_gazole,
@@ -108,11 +117,11 @@ fetch(apiUrl) //Récupere les données de l'API
           {
             label: "Moyennes des prix carburants",
             data: [
-              average(priceGazole),
+              average(priceGazole), // Appelle de la fonction average afin de faire la moyenne des valeurs prise dans le tableau
               average(priceSp95),
               average(priceSp98),
               average(priceE10),
-            ], // Les données seront remplies lorsque la première requête à l'API sera effectuée
+            ],
             backgroundColor: [
               "rgba(0,191,255, 0.6)",
               "rgba(30,144,255, 0.6)",
@@ -154,7 +163,7 @@ fetch(apiUrl) //Récupere les données de l'API
         datasets: [
           {
             label: "Blois",
-            data: [1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1], // Les données seront remplies lorsque la première requête à l'API sera effectuée
+            data: [1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1], // Entré des valeurs à la main car il s'agit d'une charte avec des valeures binaires
             backgroundColor: ["rgba(171, 58, 183, 0.4)"],
             borderColor: ["rgba(171, 58, 183, 1.0)"],
             borderWidth: 1,
@@ -184,14 +193,16 @@ fetch(apiUrl) //Récupere les données de l'API
       },
       options: {
         responsive: true,
-        maintainAspectRatio: false,
+        maintainAspectRatio: false, // Permet de supprimer le rapport de forme du graphique donc il ne sera pas maintenu et peut être librement ajusté.
         scales: {
+          // Rapport de la manière dont les données du graphique doivent être mis à l'echelle
           r: {
+            // Permet de définir un graphique radial donc une disposition adapté à cela
             angleLines: {
-              display: false,
+              display: false, // Signifie que les ligne angulaire  ne seront pas afficher
             },
             suggestedMin: 0,
-            suggestedMax: 1,
+            suggestedMax: 1, // Permet de définir la valeur minimum et maximum
           },
         },
       },
@@ -237,7 +248,6 @@ fetch(apiUrl) //Récupere les données de l'API
               "rgba(58, 183, 183, 1.0)",
             ],
             borderWidth: 2,
-            pointLabelFontColor: "rgba(255, 255, 255)",
           },
         ],
       },
@@ -250,4 +260,4 @@ fetch(apiUrl) //Récupere les données de l'API
 
   .catch((error) => {
     console.error(error);
-  });
+  }); // Permet d'écrire une erreur dans la console lorsque l'API n'arrive pas être chargé
